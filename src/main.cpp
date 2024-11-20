0fcdab4d516aadfdf9da5e0c017dc7f1aa909950
Cyril Engmann <cyengmann@gmail.com>
2024-11-20 15:23:47 -0500

    chore:init project

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <lvgl.h>
+#include <SPIFFS.h>
+// uncomment a library for display driver
+#define USE_TFT_ESPI_LIBRARY
+// #define USE_ARDUINO_GFX_LIBRARY
 
-// put function declarations here:
-int myFunction(int, int);
+#include "lv_xiao_round_screen.h"
+#include "lv_hardware_test.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200); // prepare for possible serial debug
+  Serial.println("XIAO round screen - LVGL_Arduino");
+
+  lv_init();
+
+  lv_xiao_disp_init();
+  lv_xiao_touch_init();
+
+  lv_hardware_test();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  lv_timer_handler(); // let the GUI do its work
+  delay(5);
 }
